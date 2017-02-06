7c9dbd9eaf7e9b1cd60de02b7e7183485aede3b9
Sean-Lan <sean.lan.thu@gmail.com>
2017-02-06 10:07:46 +0800

    feature: add redux dev tool support

--- common/store/configureStore.js
+++ common/store/configureStore.js
@@ -1,15 +1,25 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
 import * as head from '../components/Head';
 import * as body from '../components/Body';
 import * as footer from '../components/Footer';
 
 const rootReducer = combineReducers({head: head.reducer, body: body.reducer, footer: footer.reducer});
+
+const composeEnhancers =
+  typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk)
+);
+
 const configureStore = (preloadedState) => {
   const store = createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk)
+    enhancer
   );
   return store;
 };
